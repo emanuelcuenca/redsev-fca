6b2c71471d2bce7a33621bd9c0bad1e8a46b65be
Emanuel Cuenca <emanuelcuenca310@gmail.com>
2026-02-22 19:54:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1591734897-74b97",
+  "appId": "1:957661959248:web:408cc98776a9d5889ced55",
+  "apiKey": "AIzaSyDZZAdRqDm-SxSuVXlBoWqlX4WvbhMSI5w",
+  "authDomain": "studio-1591734897-74b97.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "957661959248"
+};
